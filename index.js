ec19f7fb71afc8f781bef1a4a33167ed3a7bfd39
Héctor Aguilar <hector-av1@hotmail.com>
2023-06-18 08:37:09 -0700

    json file values in html

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        const category = data[0].category
+        categoryReaction = data[0]
+        categoryMemory = data[1]
+        categoryVerbal = data[2]
+        categoryVisual = data[3]
         console.log(data)
-        console.log(category)
 
-        const jsonDataElement = document.getElementById('json-data')
-        jsonDataElement.innerHTML = `${category}`
+        // Reaction category article
+        const jsonReactionElement = document.getElementById('reaction')
+        jsonReactionElement.innerHTML = 
+        `
+        <div class="reaction">
+            <img src="${categoryReaction.icon}" alt="Reaction icon">
+            <div>${categoryReaction.category}</div>
+        </div>
+        <div class="points"> <p><strong>${categoryReaction.score}</strong> / 100</p> </div>
+        `
+        
+        // Memory category article
+        const jsonMemoryElement = document.getElementById('memory')
+        jsonMemoryElement.innerHTML = 
+        `
+        <div class="memory">
+            <img src="${categoryMemory.icon}" alt="Reaction icon">
+            <div>${categoryMemory.category}</div>
+        </div>
+        <div class="points"> <p><strong>${categoryMemory.score}</strong> / 100</p> </div>
+        `
+
+        // Verbal category article
+        const jsonVerbalElement = document.getElementById('verbal')
+        jsonVerbalElement.innerHTML = 
+        `
+        <div class="verbal">
+            <img src="${categoryVerbal.icon}" alt="Reaction icon">
+            <div>${categoryVerbal.category}</div>
+        </div>
+        <div class="points"> <p><strong>${categoryVerbal.score}</strong> / 100</p> </div>
+        `
+
+        // Visual category article
+        const jsonVisualElement = document.getElementById('visual')
+        jsonVisualElement.innerHTML = 
+        `
+        <div class="visual">
+            <img src="${categoryVisual.icon}" alt="Reaction icon">
+            <div>${categoryVisual.category}</div>
+        </div>
+        <div class="points"> <p><strong>${categoryVisual.score}</strong> / 100</p> </div>
+        `
     })
     .catch(error => {
         console.error('Error:', error);
